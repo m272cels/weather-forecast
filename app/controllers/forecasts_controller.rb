3731cccbb47ceeca9da0a3d1ffe44ac96f7fcb94
Kelvin Lu <kelvin.lu.sj@gmail.com>
2026-02-13 13:52:18 -0800

    consolidate search with show
    also use turbo frames for inline UI updates

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -2,20 +2,15 @@ class ForecastsController < ApplicationController
   def index
   end
 
-  def show
-    @address = params["address"]
-    @zip = params["zip"]
+  def search
+    @address = params["query"]
+    geocoded_location = ::Geocoder.search(@address).first
+    @zip = geocoded_location.postal_code
 
     @from_cache = true
     @current_weather = Rails.cache.fetch("weather_service:#{@zip}", expires_in: 30.minutes) do
       @from_cache = false
       WeatherService.new(@address).get_current_weather
     end
-  end
-
-  def search
-    address = params["query"]
-    geocoded_location = ::Geocoder.search(address).first
-    redirect_to action: :show, zip: geocoded_location.postal_code, address:
   end
 end
